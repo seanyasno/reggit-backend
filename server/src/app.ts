33d106aaf9b159e140bb4b97ee26b65284c87f64
Sean <sean.yasno@gmail.com>
2020-06-13 02:22:23 +0300

    Made a connection to db.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,24 +1,32 @@
 import {authentication} from './routes';
+import mongoose from 'mongoose';
 import express from 'express';
 import helmet from 'helmet';
+import dotenv from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
 
+dotenv.config();
 const app = express();
 
 // init
 app.use(morgan('common'));
 app.use(helmet());
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: process.env.CORS_ORIGIN
 }));
 app.use(express.json());
 
 // routes
 app.use('/api/auth', authentication)
 
+// db connection
+mongoose.connect(process.env.MONGO_DB_CONNECTION_URL || '', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => console.log('Connected to DB.'));
+
 // listen
-const PORT = 1337;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT || 8080, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
--- server/src/controllers/login-controller.ts
+++ server/src/controllers/login-controller.ts
@@ -1,5 +1,5 @@
-import {Request, Response} from 'express';
 import {UserAuth, validateUser} from '../models';
+import {Request, Response} from 'express';
 
 export default class LoginController {
     login(request: Request, response: Response) {
