c1ad87a942b8836bd0d1cf7fbba33714ef83e53d
Sean <sean.yasno@gmail.com>
2020-07-17 22:00:55 +0300

    Worked on comments API. You can now create new comment and get all comments of a specific post.

--- server/src/controllers/comment-controller.ts
+++ server/src/controllers/comment-controller.ts
@@ -1,3 +1,38 @@
-export default class CommentController {
+import {Request, Response} from 'express';
+import {CommentModel} from '../models';
 
+export default class CommentController {
+    async createComment(request: Request, response: Response) {
+        const {postId} = request.params;
+        const {userId, content} = request.body;
+
+        try {
+            const commentData = await CommentModel.create({
+                postId,
+                userId,
+                content
+            });
+            const comment = commentData?.toJSON();
+            return response.json(comment);
+        } catch (error) {
+            response.status(400).json({
+                errors: {form: error.toString()}
+            });
+        }
+    }
+
+    async getAllCommentsOfPost(request: Request, response: Response) {
+        const {postId} = request.params;
+
+        try {
+            const comments = await CommentModel.findAll({
+                where: {postId}
+            });
+            return response.json(comments);
+        } catch (error) {
+            response.status(400).json({
+                errors: {form: error.toString()}
+            });
+        }
+    }
 }
--- server/src/routes/commenting.ts
+++ server/src/routes/commenting.ts
@@ -1,7 +1,10 @@
-import {Router} from 'express';
 import CommentController from '../controllers/comment-controller';
+import {Router} from 'express';
 
 const router = Router();
 const commentController = new CommentController();
+
+router.post('/new/:postId', commentController.createComment);
+router.get('/:postId', commentController.getAllCommentsOfPost);
 
 export default router;
