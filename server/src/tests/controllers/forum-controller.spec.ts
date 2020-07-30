30a202afbdb41afbf8178cfac8c9df4e0de08751
Sean Yasnogorodski <sean.yasno@gmail.com>
2020-07-31 01:19:24 +0300

    Added get all posts by forum id test.

--- server/src/tests/controllers/forum-controller.spec.ts
+++ server/src/tests/controllers/forum-controller.spec.ts
@@ -10,6 +10,7 @@ const config = Config.getInstance().getConfiguration().ROUTES.FORUMS;
 const baseUrl = config.BASE;
 
 const forumId = '58062e83-5052-4b8d-a174-317f8ffe6dc1';
+const wrongForumId = 'wrong';
 
 describe('forum controller', () => {
     it('Get all forums', done => {
@@ -29,6 +30,17 @@ describe('forum controller', () => {
                 response.should.have.status(200);
                 response.body.should.be.a('object');
                 response.body.should.have.keys('id', 'name', 'description');
+                done();
+            });
+    });
+
+    it('Get all posts by forum id', done => {
+        chai.request(app)
+            .get(baseUrl + config.GET_ALL_POSTS_BY_FORUM_ID + forumId)
+            .end((error, response) => {
+                response.should.have.status(200);
+                response.body.should.have.be.an('array');
+                console.log(response.body);
                 done();
             });
     });
