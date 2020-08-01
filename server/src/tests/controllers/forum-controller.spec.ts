1471634aa094d07c9bceab6b8a086ead7a73a4f0
Sean Yasnogorodski <sean.yasno@gmail.com>
2020-08-01 19:36:16 +0300

    Refactored forum controller class to use async await

--- server/src/tests/controllers/forum-controller.spec.ts
+++ server/src/tests/controllers/forum-controller.spec.ts
@@ -13,55 +13,35 @@ const forumId = '58062e83-5052-4b8d-a174-317f8ffe6dc1';
 const wrongForumId = 'wrong';
 
 describe('Forum controller', () => {
-    it('Get all forums', done => {
-        chai.request(app)
-            .get(baseUrl + config.GET_ALL_FORUMS)
-            .end((error, response) => {
-                response.should.have.status(200);
-                response.body.should.be.an('array');
-                console.log(response.body);
-                done();
-            });
+    it('Get all forums', async () => {
+        const response = await chai.request(app).get(baseUrl + config.GET_ALL_FORUMS);
+        response.should.have.status(200);
+        response.body.should.be.an('array');
+        console.log(response.body);
     });
 
-    it('Get forum by id', done => {
-        chai.request(app)
-            .get(baseUrl + config.GET_FORUM_BY_ID + forumId)
-            .end((error, response) => {
-                response.should.have.status(200);
-                response.body.should.be.a('object');
-                response.body.should.have.keys('id', 'name', 'description');
-                console.log(response.body);
-                done();
-            });
+    it('Get forum by id', async () => {
+        const response = await chai.request(app).get(baseUrl + config.GET_FORUM_BY_ID + forumId);
+        response.should.have.status(200);
+        response.body.should.be.a('object');
+        response.body.should.have.keys('id', 'name', 'description');
+        console.log(response.body);
     });
 
-    it('Get all posts by forum id', done => {
-        chai.request(app)
-            .get(baseUrl + config.GET_ALL_POSTS_BY_FORUM_ID + forumId)
-            .end((error, response) => {
-                response.should.have.status(200);
-                response.body.should.have.be.an('array');
-                console.log(response.body);
-                done();
-            });
+    it('Get all posts by forum id', async () => {
+        const response = await chai.request(app).get(baseUrl + config.GET_ALL_POSTS_BY_FORUM_ID + forumId);
+        response.should.have.status(200);
+        response.body.should.have.be.an('array');
+        console.log(response.body);
     });
 
-    it('Fail at getting forum with invalid id', done => {
-        chai.request(app)
-            .get(baseUrl + config.GET_FORUM_BY_ID + wrongForumId)
-            .end((error, response) => {
-                response.should.have.status(404);
-                done();
-            });
+    it('Fail at getting forum with invalid id', async () => {
+        const response = await chai.request(app).get(baseUrl + config.GET_FORUM_BY_ID + wrongForumId);
+        response.should.have.status(404);
     });
 
-    it('Fail at getting posts with invalid forum id', done => {
-        chai.request(app)
-            .get(baseUrl + config.GET_ALL_POSTS_BY_FORUM_ID + wrongForumId)
-            .end((error, response) => {
-                response.should.have.status(400);
-                done();
-            });
+    it('Fail at getting posts with invalid forum id', async () => {
+        const response = await chai.request(app).get(baseUrl + config.GET_ALL_POSTS_BY_FORUM_ID + wrongForumId);
+        response.should.have.status(400);
     });
 });
